197635fbb605ea491118119fdce115850d402097
NikolaPatafta <44970174+NikolaPatafta@users.noreply.github.com>
2023-03-09 21:39:39 +0100

    Inventory switchable weapons

--- A-Week-To-Survive/Assets/SCRIPTS/Scriptable Items/EquipmentManager.cs
+++ A-Week-To-Survive/Assets/SCRIPTS/Scriptable Items/EquipmentManager.cs
@@ -4,86 +4,72 @@ using UnityEngine;
 
 public class EquipmentManager : MonoBehaviour
 {
-
+    
     [SerializeField]
     private Transform WeaponHolder = null;
 
-    private Inventory inventory;
+    [SerializeField]
+    private InventoryManager inventoryManager;
 
     //Equipment manager - Animations for hands https://www.youtube.com/watch?v=HrTrx_98e8s
 
-    private Animator anim;
+    private Animator anim = null;
 
-    private GameObject currentWeaponObject = null;
+    [SerializeField]
+    private GameObject[] Toolbar;
+
+    [SerializeField]
+    private GameObject currentlyEquipedWeapon;
+
+    private int currentlySelectedSlot;
+
+    [SerializeField]
+    Weapons defaultweapon = null;
 
     private void Start()
     {
-        inventory = GetComponent<Inventory>();
-        anim = GetComponentInChildren<Animator>();
-
+        //anim = GetComponentInChildren<Animator>();
+        //Debug.Log("animator: " + anim.name);
     }
 
     private void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Alpha7))
+        if(Input.GetKeyUp(KeyCode.Alpha1))
         {
-            //if AR is equiped play AR animations
-            ChooseWeaponAnimations(0, WeaponType.AR);
-
-            EquipWeapon(inventory.GetWeapons(0).prefab, 0);
+            EquipWeapon(inventoryManager.GetCurrentlySelectedWeapon());
         }
-        if(Input.GetKeyUp(KeyCode.Alpha8))
+        if(Input.GetKeyUp(KeyCode.Alpha2))
         {
-            //if Pistol is equiped play pistol animations
-            ChooseWeaponAnimations(1, WeaponType.Pistol);
-
-            EquipWeapon(inventory.GetWeapons(1).prefab, 1);
+            EquipWeapon(inventoryManager.GetCurrentlySelectedWeapon());
 
         }
-        if(Input.GetKeyUp(KeyCode.Alpha9))
+        if(Input.GetKeyUp(KeyCode.Alpha3))
         {
-            //if Knife is equiped play knife animations
-            ChooseWeaponAnimations(2, WeaponType.Melee);
-
-            EquipWeapon(inventory.GetWeapons(2).prefab, 2);
+            EquipWeapon(inventoryManager.GetCurrentlySelectedWeapon());
 
         }
     }
 
-    private void ChooseWeaponAnimations(int weaponStyle, WeaponType weaponType)
+    private void EquipWeapon(Weapons weapon)
     {
-        Weapons weapons = inventory.GetWeapons(weaponStyle);
-
-        if (weapons != null)
+        if (WeaponHolder.GetComponentInChildren<Animator>() != null)
         {
-            if (weapons.weaponType == weaponType)
-            {
-                anim.SetInteger("weaponType", (int)weaponType);
-            }
+            Debug.Log("Weapon Unequiped");
+            UnequipWeapon(); 
         }
+        else
+        {
+            currentlyEquipedWeapon = Instantiate(weapon.prefab, WeaponHolder);
+            Debug.Log("Instantiated: " + currentlyEquipedWeapon);
+        }
+          
     }
 
-    private void InstantiateWeapon(GameObject weaponObj, int weaponStyle)
+    private void UnequipWeapon() 
     {
-        Weapons weapon = inventory.GetWeapons(weaponStyle);
-        if (weapon != null)
+        if (currentlyEquipedWeapon != null )
         {
-            Instantiate(weaponObj, WeaponHolder);
+            Destroy(currentlyEquipedWeapon);
         }
-    }
-
-    private void EquipWeapon(GameObject weaponObject, int weaponStyle)
-    {
-        Weapons weapons = inventory.GetWeapons(weaponStyle);
-        if (weapons != null)
-        {
-            currentWeaponObject = Instantiate(weaponObject, WeaponHolder);
-        }
-    }
-
-    private void UnequipWeapon()
-    {
-        anim.SetTrigger("unequipTrigger");
-        Destroy(currentWeaponObject);
     }
 }
--- A-Week-To-Survive/Assets/SCRIPTS/WeaponsScripts/WeaponManager.cs
+++ A-Week-To-Survive/Assets/SCRIPTS/WeaponsScripts/WeaponManager.cs
@@ -15,9 +15,12 @@ public class WeaponManager : MonoBehaviour
 
     private int current_Weapon_Index;
 
+    public Items CurrentlySelctedItem; 
+
     // Start is called before the first frame update
     void Start()
     {
+        CurrentlySelctedItem = inventoryManager.GetCurrentlySelectedItem();
         //current_Weapon_Index = 0;
         //weapons[current_Weapon_Index].gameObject.SetActive(true);
         //TurnOnSelectedWeapon(6);      
@@ -26,64 +29,13 @@ public class WeaponManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            ActivateInventoryItemInHotbar(0);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ActivateInventoryItemInHotbar(1);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ActivateInventoryItemInHotbar(2);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            ActivateInventoryItemInHotbar(3);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            ActivateInventoryItemInHotbar(4);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            ActivateInventoryItemInHotbar(5);
-        }
-        //ova dva ostala po starom (turnonselectedweapon) metodi
-        if (Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            TurnOnSelectedWeapon(6);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha8))
-        {
-            TurnOnSelectedWeapon(7);
-        }
 
-    }
-
-    void TurnOnSelectedWeapon(int weaponIndex)
-    {
-        if (current_Weapon_Index == weaponIndex)
-        {
-            return;
-        }
-        //unequip current weapon
-        weapons[current_Weapon_Index].gameObject.SetActive(false);
-
-        //equip selected weapon
-        weapons[weaponIndex].gameObject.SetActive(true);
-
-        current_Weapon_Index = weaponIndex; 
     }
 
     public WeaponHandler GetCurrentSelectedWeapon()
     {
         return weapons[current_Weapon_Index];
     }
+  
 
-    private void ActivateInventoryItemInHotbar(int index)
-    {
-        Instantiate(inventoryManager.GetCurrentlySelectedItem().prefab, weaponHolderPosition);
-    }
 }
